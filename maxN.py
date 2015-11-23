49efd697dd20fe3f2d9ae14319cdff322f99ea7c
mdgoldberg <mgoldberg@college.harvard.edu>
2015-11-23 17:29:54 -0500

    first attempt at maxN

--- maxN.py
+++ maxN.py
@@ -1,8 +1,10 @@
 from collections import Counter
 import operator as op
+import time
 
 import agent
 import cards
+import game
 
 class MaxNAgent(agent.Agent):
 
@@ -17,31 +19,76 @@ class MaxNAgent(agent.Agent):
         """Chooses a move by (TODO: explain max-n)
 
         :state: The current state from which we make a move.
-        :returns: The (numCards, whichCard) action pair.
+        :returns: The (numCards, whichCard) action pair and the values of the
+        node for each player.
         """
-        bestAction = Counter()
         allActions = self.getAllActions(state)
+        if len(allActions) == 1:
+            return allActions[0]
+        totalPlayed = sum(sum(played.values()) for played in state.playedCards)
+        bestActions = Counter()
+        # subtract played cards and your own hand from cards remaining
         cardsLeft = cards.allCards()
         for played in state.playedCards:
             cardsLeft = cards.diff(cardsLeft, played)
         cardsLeft = cards.diff(cardsLeft, self.hand)
+
+        # sample opponent hands
+        print 'trialing', state.topCard, self.hand
         initHandSize = 52 / state.numPlayers
-        for trial in xrange(100):
+        for trial in xrange(25):
+            print 'trial', trial
             hands = cards.dealHands(
                 cardsLeft, 
                 [ 
                     initHandSize - sum(played.itervalues())
                     for i, played in enumerate(state.playedCards) 
-                    if i != self.idx
+                    if i != self.idx # don't give hand for current player
                 ]
             )
             hands.insert(self.idx, self.hand)
-            # now `hands` holds the current players' hands; do the trial now
-            # using perfect information max-N
-            # ...
-            best = allActions[-1]
-            bestAction[best] += 1
+            agents = map(lambda (i,h): MaxNAgent(i, h),
+                         zip(xrange(state.numPlayers), hands))
+            bestAct, bestVals = maxN(state, self, agents, 0, state.numPlayers*2)
+            bestActions[bestAct] += 1
         
         # get most frequent best action and return
-        allBest = max(bestAction, key=bestAction.get)
+        allBest = max(bestActions, key=bestActions.get)
+        print allBest, bestActions
         return allBest
+
+
+def maxN(state, player, agents, d, maxDepth):
+    numPlayers = len(agents)
+    # returns act, val
+    allActions = player.getAllActions(state)
+    if d > maxDepth:
+        # heuristic - take action that maximizes cards played
+        bestAct = None
+        bestVals = [-float('inf') for p in xrange(numPlayers)]
+        for possAct in allActions:
+            child = state.getChild(possAct)
+            cardsPlayed = [sum(played.itervalues())
+                           for played in state.playedCards]
+            if cardsPlayed[player.idx] > bestVals[player.idx]:
+                bestAct = possAct
+                bestVals = cardsPlayed
+        return bestAct, bestVals
+    if state.isFinalState():
+        assert sum(player.hand.values()) == 0
+        return (0, 0), [numPlayers - state.finished.index(player) 
+                        if player in state.finished else numPlayers - 1
+                        for player in xrange(numPlayers)]
+    bestAct = None
+    bestVals = [-float('inf') for i in xrange(numPlayers)]
+    for possAct in allActions:
+        numCards, whichCard = possAct
+        child = state.getChild(possAct)
+        nextPlayer = agents[child.whosTurn]
+        nextAllActions = nextPlayer.getAllActions(child)
+        hisBestAct, hisVals = maxN(child, nextPlayer, agents, d+1, maxDepth)
+        if hisVals[player.idx] > bestVals[player.idx]:
+            bestVals = hisVals
+            bestAct = possAct
+
+    return bestAct, bestVals
