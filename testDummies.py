59238d9ae01dd5ea8e1f479e95d4fac2667e1286
mdgoldberg <mgoldberg@college.harvard.edu>
2015-12-03 01:02:19 -0500

    put in testDummies to test the dummy agents and the game module

--- testDummies.py
+++ testDummies.py
@@ -1,6 +1,6 @@
 import game, maxN, dummyAgent
 
-gm = game.Game([dummyAgent.DummyAgent]*4)
+gm = game.Game([dummyAgent.DummyAgent for i in xrange(4)])
 print [sum(k*v for k, v in agent.hand.items()) for agent in gm.agents]
 print gm.agents[gm.initialState.whosTurn].hand
 print gm.playGame()
