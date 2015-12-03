4b207aad35dd3dbc1e4ce6fde2ebc8c3f88e4de6
mdgoldberg <mgoldberg@college.harvard.edu>
2015-12-03 01:45:42 -0500

    testDummies change

--- testDummies.py
+++ testDummies.py
@@ -1,6 +1,7 @@
 import game, maxN, dummyAgent
 
 gm = game.Game([dummyAgent.DummyAgent for i in xrange(4)])
-print [sum(k*v for k, v in agent.hand.items()) for agent in gm.agents]
-print gm.agents[gm.initialState.whosTurn].hand
+for agent in gm.agents:
+    print sum(k*v for k, v in agent.hand.items()),
+    print agent.hand
 print gm.playGame()
