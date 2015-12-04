4e2c3e5f3d21e14398d8c77f0b8ac4893ca694da
mdgoldberg <mgoldberg@college.harvard.edu>
2015-12-04 00:17:38 -0500

    added verbose flag to test files

--- testDummies.py
+++ testDummies.py
@@ -1,6 +1,6 @@
 import game, maxN, dummyAgent
 
-gm = game.Game([dummyAgent.DummyAgent for i in xrange(4)])
+gm = game.Game([dummyAgent.DummyAgent for i in xrange(4)], verbose=True)
 for agent in gm.agents:
     print sum(k*v for k, v in agent.hand.items()),
     print agent.hand
