4b26bd54ca2d0a576efc2c56b65ade5c1213d1f6
mdgoldberg <mgoldberg@college.harvard.edu>
2015-12-04 00:16:52 -0500

    added verbose setting to game

--- game.py
+++ game.py
@@ -12,7 +12,7 @@ class Game(object):
     from players and organizing play."""
 
     def __init__(self, agents, hands=None, playedCards=None, whosTurn=None,
-                 topCard=None, lastPlayed=None, finished=[]):
+                 topCard=None, lastPlayed=None, finished=[], verbose=False):
         """Initializes the game with the agents listed as the players.
 
         :agents: Either a list of agent objects or a list of agent
@@ -24,6 +24,7 @@ class Game(object):
         the playedCards for the initialState.
         :whosTurn: index of agent whose turn it is; if supplied, the game will
         start with this player.
+        :verbose: If True, playGame will print each move made by each player.
 
         topCard, lastPlayed, and finished are all parameters passed on to the
         initial state; for more information, see state.State's __init__.
@@ -53,6 +54,7 @@ class Game(object):
         if playedCards is None:
             playedCards = [cards.noCards() for i in xrange(self.numPlayers)]
 
+        self.verbose = verbose
         self.initialState = state.State(playedCards, whosTurn, topCard,
                                         lastPlayed, finished)
 
@@ -80,6 +82,8 @@ class Game(object):
             agentToMove = self.agents[whosTurn]
             # get the move - ask agent for move
             (numCards, whichCard) = agentToMove.makeMove(curState)
+            if self.verbose:
+                print whosTurn, numCards, cards.cardRepr[whichCard]
             # make the move by taking cards out of hand, if not a pass
             if numCards > 0:
                 agentToMove.hand[whichCard] -= numCards
