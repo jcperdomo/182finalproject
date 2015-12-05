f1eb632282d12b0135e5e1dece7bc625bdb65b44
mdgoldberg <mgoldberg@college.harvard.edu>
2015-12-05 04:56:44 -0500

    small fix to hand printing in manVsMachine at the beginning

--- manVsMachine.py
+++ manVsMachine.py
@@ -1,11 +1,12 @@
 import copy
 
-import game
+import cards, game
 import humanAgent, maxN, dummyAgent
 
 gm = game.Game([humanAgent.HumanAgent] + [maxN.MaxNAgent for i in xrange(3)])
 origHands = [copy.deepcopy(agent.hand) for agent in gm.agents]
-print 'Your hand:', origHands[0]
+print 'Your hand:', map(lambda c: cards.cardRepr[c],
+                        cards.cardDictToList(origHands[0]))
 print gm.playGame(verbose=True)
 print 'Original hands and strengths:'
 for hand in origHands:
