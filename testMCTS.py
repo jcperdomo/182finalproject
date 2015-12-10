f82846a8c07348e849edceb4ec7a5898661b6736
Juan Perdomo <jperdomo@college.harvard.edu>
2015-12-09 20:00:15 -0500

    wrote background related sources, division of tasks and results for MCTS

--- testMCTS.py
+++ testMCTS.py
@@ -3,22 +3,38 @@ import cards
 import state
 import dummyAgent
 import game
-"""
-NP = 5
+import operator
 
-hands  = cards.dealHands(cards.allCards(), [5 for i in xrange(NP)])
 
-print
-testState  = state.State([cards.noCards() for i in xrange(NP)], 0)
-agent  = mcts.mctsAgent(0, hands[0])
-printHand = cards.cardDictToList(agent.hand)
-printHand = map(lambda c: cards.cardRepr[c], printHand)
-#print "Agent Hand:", printHand
+hands = [{0: 1, 1: 0, 2: 1, 3: 1, 4: 0, 5: 1, 6: 0, 7: 0, 8: 2, 9: 0, 10: 1, 11: 0, 12: 3},
+{0: 0, 1: 0, 2: 1, 3: 0, 4: 0, 5: 1, 6: 2, 7: 2, 8: 2, 9: 1, 10: 0, 11: 0, 12: 1},
+{0: 1, 1: 0, 2: 1, 3: 1, 4: 3, 5: 1, 6: 1, 7: 0, 8: 0, 9: 0, 10: 0, 11: 2, 12: 0},
+{0: 2, 1: 1, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 2, 8: 0, 9: 2, 10: 2, 11: 1, 12: 0},
+{0: 0, 1: 3, 2: 1, 3: 2, 4: 1, 5: 0, 6: 0, 7: 0, 8: 0, 9: 1, 10: 1, 11: 1, 12: 0}]
 
-print "Chosen Move: ", agent.makeMove(testState)"""
-
-gm = game.Game([mcts.mctsAgent] + [dummyAgent.DummyAgent for i in xrange(4)])
+gm = game.Game([mcts.mctsAgent] + [dummyAgent.DummyAgent for i in xrange(4)], hands)
 for agent in gm.agents:
     print sum(k*v for k, v in agent.hand.items()),
     print agent.hand
 print gm.playGame(verbose = True)
+
+
+"""
+numGames = 20
+numPlayers = 4
+gm = game.Game([mcts.mctsAgent] + [dummyAgent.DummyAgent for i in xrange(numPlayers - 1)])
+#for agent in gm.agents:
+#    print sum(k*v for k, v in agent.hand.items()),
+#    print agent.hand
+#print gm.playGame(verbose=True)
+#print gm.agents[0].nodeList
+res = gm.playMultGames(verbose=False, n=numGames)
+print '-------------------------------------------'
+#print 'Average nodes expanded after {0} games: {1:.5}'.format(numGames, sum(gm.agents[0].nodeList) / float(numGames))
+rankings = reduce(lambda x, y: map(operator.add, x, y), res)
+print '--------------------------------'
+print 'Average rankings after {0} games'.format(numGames)
+print '--------------------------------'
+for player in xrange(numPlayers):
+    print 'Player {0}: {1:.5}'.format(player, rankings[player]/float(numGames)),
+"""
